84b3f4209123789512425ce010cb68866175310c
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-04-15 22:11:26 +0100

    feat(go/search_algorithms/): implement interpolation search

---
+++ go/search_algorithms/interpolation_search.go
@@ -0,0 +1,51 @@
+package searchalgorithms
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// InterpolationSearch is a search algorithm that finds a value in a sorted slice of values.
+//
+// It takes a sorted slice of integers and the value to search for
+// and returns the index of the first occurence of the value in the slice
+// or “NotFound“ if the value is not found.
+func InterpolationSearch(slice []int, value int) (int, error) {
+	// Set up logging file
+	logFile, err := os.OpenFile("/tmp/searchalgorithms.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return 0, fmt.Errorf("InterpolationSearch failed to open file: %w", err)
+	}
+
+	log.SetOutput(logFile)
+	log.Printf("Performing Interpolation Search on slice %v\n", slice)
+
+	l_bound := 0
+	u_bound := len(slice) - 1
+
+	// Use interpolation formula to find the index of the value
+	for l_bound <= u_bound && slice[l_bound] < value && slice[u_bound] >= value {
+		pos := l_bound + ((u_bound-l_bound)*(value-slice[l_bound]))/(slice[u_bound]-slice[l_bound])
+		log.Printf("Value at index [%d] = [%d]\n", pos, slice[pos])
+
+		switch {
+		case slice[pos] > value:
+			u_bound = pos - 1
+		case slice[pos] < value:
+			l_bound = pos + 1
+		default:
+			// Make sure it is the first occurence
+			for pos > 0 && slice[pos-1] == value {
+				pos--
+			}
+			log.Printf("Value [%v] found at index [%v]\n\n", value, pos)
+			return pos, nil
+		}
+	}
+
+	// Value was not found
+	log.Printf("Value [%v] not found in slice\n\n", value)
+	logFile.Close()
+	return NotFound, nil
+}
