bdfc6bd216323ee764fbfbe4da5d797a8ea8f0d1
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-04-15 13:45:26 +0100

    docs(go/search_algorithms/doc.go): add `searchalgorithms` package doc

---
+++ go/search_algorithms/doc.go
@@ -0,0 +1,6 @@
+// Package searchalgorithms implements many of the most used search algorithms
+// using the Go programming language.
+//
+// It utilizes the latest and most efficient techniques like generics in order
+// to make functions work for as many types as possible.
+package searchalgorithms
